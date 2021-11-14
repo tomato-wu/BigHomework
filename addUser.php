497ecd8b5c8bfe5756e14aa77855736e1fb5feda
tomato <h20333@qq.com>
2021-11-14 12:15:16 +0800

    fix:修复登录注册逻辑

--- addUser.php
+++ addUser.php
@@ -9,55 +9,27 @@ require_once("conn.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>注册界面</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" crossorigin="anonymous">
 </head>
 
 <body>
-
-
     <?php
-    if (isset($_POST['RegisterSubmit'])) { //判断是否点击注册按钮
-        $userName = $_POST['RegisterUserName'];
-        $userPass1 = $_POST['userPass1'];
-        $userPass2 = $_POST['userPass2'];
-        $name = $_POST['name'];
-        if (isset($userName)) { //判断用户名不为空
-            if (
-                isset($userPass1)
-                && isset($userPass2)
-                && $userPass1 === $userPass2
-            ) {
-                //判断两次密码相同，且不为空
-                //信息存入数据库
-                $sql = "insert into users value( '$userName','$userPass1','$name' )";
-
-                if (mysqli_query($conn, $sql)) {
-                    echo "<script>alert('注册成功，马上登录')</script>";
-                    header("Refresh:0;url=home.php");
-                } else {
-                    echo "注册失败 <br />";
-                    echo  "<a href='register.php'>请返回重新注册</a>";
-                }
-            } else {
-                echo "密码输入有误 <br />";
-                echo  "<a href='register.php'>请重新注册</a>";
-            }
-        } else {
-            echo "用户名不能空<br />";
-            echo  "<a href='./register.php'>请重新注册</a>";
-        }
+    $userName = $_POST['RegisterUserName'];
+    $userPass1 = $_POST['userPass1'];
+    $userPass2 = $_POST['userPass2'];
+    $name = $_POST['name'];
+    $sql = "insert into users value( '$userName','$userPass1','$name' )";
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('注册成功，马上登录')</script>";
+        header("Refresh:0;url=home.php");
     } else {
-        echo "请正确输入<br />";
-        header("Location:register.php");
+        echo "注册失败 <br />";
+        echo  "<a href='home.php'>请返回重新注册</a>";
     }
+
     ?>
     <?php
     mysqli_close($conn); //关闭数据库连接
     ?>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- loginUser.php
+++ loginUser.php
@@ -4,29 +4,24 @@ require_once("conn.php");
 
 <meta charset="utf-8">
 <?php
-session_start();
 
-if ($_POST['userName'] != ''  &&  $_POST['userPass'] != '') {
-
-
-	$username = $_POST['userName'];
-	$userpass = $_POST['userPass'];
-
-
-	if (isset($username) && isset($userpass)) { //判断用户名和密码均不为空
-		$sql = "SELECT * FROM users  where userName='{$_POST['userName']}' and password='{$_POST['userPass']}' ";
-		$result = mysqli_query($conn, $sql);
-		$count = mysqli_num_rows($result);
-		if ($count == 0) {
-			echo "账号密码错误，查无此人 <br />";
-			echo  "<a href='home.php'>请返回重新登录</a>";
-		} else {
-			header("Location:homepage.php");
-		}
+$username = $_POST['userName'];
+$userpass = $_POST['userPass'];
+if (isset($username) && isset($userpass)) { //判断用户名和密码均不为空
+	$sql = "SELECT * FROM users  where userName='{$_POST['userName']}' and password='{$_POST['userPass']}' ";
+	$result = mysqli_query($conn, $sql);
+	$count = mysqli_num_rows($result);
+	if ($count == 0) {
+?>
+		<script>
+			alert("密码错误")
+			window.location = "./home.php"
+		</script>
+<?php
+	} else {
+		header("Location:homepage.php");
 	}
-} else {
-	echo "账号密码不能为空 <br />";
-	echo  "<a href='home.php'>请返回重新登录</a>";
 }
+
 mysqli_close($conn);
 ?>
