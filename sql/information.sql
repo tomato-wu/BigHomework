55a8868f727f269b77fa1a1056d5038c2221f02d
Sanens <974849026@qq.com>
2021-12-01 15:18:33 +0800

    修改数据库，创建两个视图

--- sql/information.sql
+++ sql/information.sql
@@ -42,12 +42,11 @@ create table course
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-/*学生-关系*/
-create table relation
+/*学生-选课*/
+create table section
 (
     s_id varchar(20),
 	course_id varchar(20),
-	practice_id varchar(20),
 	score int,
 	primary key(s_id,course_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -64,9 +63,16 @@ create table social_practice
 	primary key(practice_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+create table student_social_practice
+(	
+	s_id varchar(20),
+	practice_id varchar(20),
+	primary key(practice_id,s_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+create view  student_course as select s_id,course_id,course_name,semester,year,score from student natural join course natural join section;
+create view  student_practice as select s_id,practice_id,practice_experience,date,place,pracetice_time,instructor  from student natural join social_practice natural join student_social_practice;
 
-create view  s_relation as select  * from student natural join course natural join social_practice natural join relation;
 
 /*插入数据*/
 /*学生*/
@@ -92,6 +98,14 @@ insert into course values('法律基础','003','Fall','2019');
 insert into course values('微观经济学','004','Fall','2020');
 insert into course values('宏观经济学','005','Spring','2019');
 
+/*学生-选课*/
+insert into section values('201909101','001',98);
+insert into section values('201909103','001',92);
+insert into section values('201909104','003',96);
+insert into section values('201909105','004',96);
+insert into section values('201909106','005',80);
+insert into section values('201909107','006',69);
+
 
 
 /*社会实践*/
@@ -102,11 +116,11 @@ insert into social_practice values('004','省长杯',' 4月','广外','30','Baby
 insert into social_practice values('005','亚运会','暑假','学生高中','30','Stark');
 insert into social_practice values('006','广交会','暑假','广州琶洲','30','Baby');
 insert into social_practice values('007','社区核酸检测支援','寒假','小谷围街道','30','Stark');
-
-/*学生-关系*/
-insert into relation values('201909101','001','001',98);
-insert into relation values('201909103','001','002',92);
-insert into relation values('201909104','002','003',96);
-insert into relation values('201909105','002','004',96);
-insert into relation values('201909106','Acca002','005',80);
-insert into relation values('201909107','Acca001','006',69);
+/*学生-社会实践*/
+insert into student_social_practice values('201909101','001');
+insert into student_social_practice values('201909102','001');
+insert into student_social_practice values('201909103','003');
+insert into student_social_practice values('201909104','002');
+insert into student_social_practice values('201909105','005');
+insert into student_social_practice values('201909106','006');
+insert into student_social_practice values('201909107','007');
