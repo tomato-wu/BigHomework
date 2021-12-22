14b5fccdd533874b512570e124b232e0d6cfb86f
tomato <h20333@qq.com>
2021-12-22 21:28:25 +0800

    fix:注册md5加密

--- addUser.php
+++ addUser.php
@@ -16,8 +16,9 @@ require_once("conn.php");
     $userName = $_POST['RegisterUserName'];
     $userPass1 = $_POST['userPass1'];
     $userPass2 = $_POST['userPass2'];
+    $md5PassWord = md5($userPass2);
     $name = $_POST['name'];
-    $sql = "insert into users value( '$userName','$userPass1','$name' )";
+    $sql = "insert into users value( '$userName','$md5PassWord','$name' )";
     if (mysqli_query($conn, $sql)) {
         echo "<script>alert('注册成功，马上登录')</script>";
         header("Refresh:0;url=home.php");
